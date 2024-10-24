571e610269f46ded088bc23fad86a54acee0d6c2
Wardega321 <sswist707@technikumkreatywne.pl>
2024-10-24 10:13:13 +0200

    Komentarz

---
+++ Lekcja 1/Zadanie_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	cout<<"Witaj w programowaniu C++!!!"<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    return 0;
+}
---
+++ Lekcja 1/Zadanie_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int zmienna_calkowita = 10;
+	float zmienna_zmiennoprzecinkowa = 3.14;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    return 0;
+}
